58a1341ab82c2714c517e65d86e2282400eefe3b
txyyddss <tx@1919801.xyz>
2026-04-16 17:31:34 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            '/api': 'http://localhost:8080'
+            '/api': 'https://mtnapi.1391399.xyz/'
         }
     }
 })
